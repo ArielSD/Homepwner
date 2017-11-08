8f9ea049a3dceeac30e0d6d43a7a6f08a1c05ff3
Ariel <ariel.scottdicker@shopify.com>
2017-11-07 22:37:51 -0500

    Initial setup and initializers

---
+++ Homepwner/ViewControllers/ItemsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Ariel Scott-Dicker on 2017-11-07.
+//  Copyright © 2017 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+}
