3ba6fee46028b5fc92ce2197a4a3726b4bee110b
Ariel <ariel.scottdicker@shopify.com>
2017-11-08 23:13:22 -0500

    Creating the ItemStore

---
+++ Homepwner/Models/ItemStore.swift
@@ -0,0 +1,14 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by Ariel Scott-Dicker on 2017-11-08.
+//  Copyright © 2017 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+	var allItems = [Item]()
+	
+}
