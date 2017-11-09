dc78098a55e6a01105b89e27f92c8beae5a9c504
Ariel <ariel.scottdicker@shopify.com>
2017-11-08 23:28:48 -0500

    Dependency Inversion

--- Homepwner/Models/ItemStore.swift
+++ Homepwner/Models/ItemStore.swift
@@ -11,4 +11,15 @@ import UIKit
 class ItemStore {
 	var allItems = [Item]()
 	
+	init() {
+		for _ in 0..<5 {
+			createItem()
+		}
+	}
+	
+	@discardableResult func createItem() -> Item {
+		let newItem = Item(random: true)
+		allItems.append(newItem)
+		return newItem
+	}
 }
