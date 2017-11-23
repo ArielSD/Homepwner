d75c8c4dd4b502f4e0ae16068b50a9ce1cb9e86a
Ariel <ariel.scottdicker@shopify.com>
2017-11-22 21:49:56 -0500

    Custom tableView cell

---
+++ Homepwner/Views/ItemCell.swift
@@ -0,0 +1,15 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Ariel Scott-Dicker on 2017-11-22.
+//  Copyright © 2017 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+	@IBOutlet var nameLabel: UILabel!
+	@IBOutlet var serialNumberLabel: UILabel!
+	@IBOutlet var valueLabel: UILabel!
+}
