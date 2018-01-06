596c3afc2e308aaa463fcd1ec97aafc97a4c40f8
Ariel <ariel.scottdicker@shopify.com>
2018-01-06 11:18:50 -0800

    Adding barButtonItems

--- Homepwner/ViewControllers/ItemsViewController.swift
+++ Homepwner/ViewControllers/ItemsViewController.swift
@@ -11,7 +11,12 @@ import UIKit
 class ItemsViewController: UITableViewController {
 	var itemStore: ItemStore!
 	
-	@IBAction func addNewItem(_ sender: UIButton) {
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		navigationItem.leftBarButtonItem = editButtonItem
+	}
+	
+	@IBAction func addNewItem(_ sender: UIBarButtonItem) {
 		// Create a new item and add it to the store
 		let newItem = itemStore.createItem()
 		
@@ -24,31 +29,8 @@ class ItemsViewController: UITableViewController {
 		}
 	}
 	
-	@IBAction func toggleEditingMode(_ sender: UIButton) {
-		// If you are currently in editing mode...
-		if isEditing {
-			// Change text of button to inform user of state
-			sender.setTitle("Edit", for: .normal)
-			
-			// Turn off editing mode
-			setEditing(false, animated: true)
-		} else {
-			// Change text of button to inform user of state
-			sender.setTitle("Done", for: .normal)
-			
-			// Enter editing mode
-			setEditing(true, animated: true)
-		}
-	}
-	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
-		// Get the height of the status bar
-		let statusBarHeight = UIApplication.shared.statusBarFrame.height
-		let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
-		tableView.contentInset = insets
-		tableView.scrollIndicatorInsets = insets
 		
 		tableView.rowHeight = UITableViewAutomaticDimension
 		tableView.estimatedRowHeight = 65
